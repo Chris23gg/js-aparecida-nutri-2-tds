4f87bc7b60fe111d4be33f10c692f885d6e4f25f
Chris23gg <160490561+Chris23gg@users.noreply.github.com>
2024-11-01 12:51:08 -0300

    carga_adicionar-dados

--- js/principal.js
+++ js/principal.js
@@ -39,4 +39,43 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
-console.log (tdImc)
+
+titulo.addEventListener("click", mostraMensagem)
+function mostraMensagem() {
+    console.log("Olá eu fui clicado!");
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar. addEventListener("click", function (event) {
+event.preventDefault();
+    // console.log(" oi, cliquei bo botao .");
+ 
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome; 
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura; 
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr)
+});
